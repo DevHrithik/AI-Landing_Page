7b7bf244d545f0f08c3be733112fc6b8087b3200
Hrithik <hrithikroy1854@gmail.com>
2024-08-14 23:23:22 +0530

    added hero section

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -1,10 +1,11 @@
 import LogoIcon from "@/assets/logo.svg";
 import MenuIcon from "@/assets/icon-menu.svg";
+import Button from "@/components/Button";
 export const Header = () => {
   return (
-    <header className="py-4 border-b border-white/15">
+    <header className="py-4 border-b border-white/15 md:border-none sticky top-0 z-10">
       <div className="container">
-        <div className="flex justify-between items-center md:border border-white/15 md:p-2.5 rounded-xl max-w-2xl">
+        <div className="flex justify-between items-center md:border border-white/15 md:p-2.5 rounded-xl max-w-2xl mx-auto backdrop-blur">
           <div>
             <div className="border border-white/15 inline-flex justify-center items-center h-10 w-10">
               <LogoIcon className="h-8 w-8" />
@@ -27,14 +28,7 @@ export const Header = () => {
             </nav>
           </div>
           <div className="flex items-center gap-4">
-            <button className="relative py-2 px-3 font-medium text-sm rounded-lg bg-gradient-to-b from-[#190d2e] to-[#4a208a] shadow-[0px_0px_12px_#8c45ff]">
-              <div className="absolute inset-0">
-                <div className="border rounded-lg border-white/20 absolute inset-0 [mask-image:linear-gradient(to_bottom,black,transparent)]"></div>
-                <div className="border rounded-lg border-white/40 absolute inset-0 [mask-image:linear-gradient(to_top,black,transparent)]"></div>
-                <div className="absolute inset-0 shadow-[0_0_10px_rgb(140,69,255,.7 )_inset]"></div>
-              </div>
-              <span> Join Waitlist</span>
-            </button>
+            <Button>Join Waitlist</Button>
             <MenuIcon className="md:hidden" />
           </div>
         </div>
